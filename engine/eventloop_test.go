bd76b325752461a3375580830988a5695a333cab
Anastasiiiii <nastia262002@gmail.com>
2022-06-26 18:44:38 +0300

    add evntloop tests

--- engine/eventloop_test.go
+++ engine/eventloop_test.go
@@ -1,1 +1,39 @@
 package engine
+
+import (
+  "strconv"
+  "testing"
+
+  "github.com/stretchr/testify/assert"
+)
+
+func TestLoop(t *testing.T) {
+  printCmd1 := &PrintCommand{
+    arg: "first test",
+  }
+  printCmd2 := &PrintCommand{
+    arg: "second test",
+  }
+  addCmd := &AddCommand{
+    arg1: 1,
+    arg2: 2,
+  }
+
+  eventLoop := new(EventLoop)
+  eventLoop.Start()
+  assert.Equal(t, false, eventLoop.stopped)
+  assert.Equal(t, 0, len(eventLoop.q.commands))
+
+  eventLoop.Post(addCmd)
+  eventLoop.Post(printCmd1)
+  eventLoop.Post(printCmd2)
+
+  assert.Equal(t, 3, len(eventLoop.q.commands))
+  eventLoop.AwaitFinish()
+  assert.Equal(t, true, eventLoop.stopped)
+  assert.Equal(t, 0, len(eventLoop.q.commands))
+
+  assert.Equal(t, printCmd1, &PrintCommand{arg: "first test"})
+  assert.Equal(t, printCmd2, &PrintCommand{arg: "second test"})
+  assert.Equal(t, strconv.Itoa(addCmd.arg1+addCmd.arg2), "3")
+}
